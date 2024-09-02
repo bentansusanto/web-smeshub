2884027fa611b9c1d4e569462411be269b003ee2
bennytansusanto <test@192.168.1.2>
2024-09-02 16:59:15 +0700

    fix error goodpeople

--- src/common/FetchData.ts
+++ src/common/FetchData.ts
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 export const baseUrl = "https://admin.smeshub.co.id";
 export const baseUrlApi = "https://admin.smeshub.co.id/api";
@@ -40,73 +40,73 @@ export const fetchData = async (url: string) => {
 // };
 
 // logic get data content cta home
-export const fetchDataCtaHome = () => {
-  const [dataHome, setDataHome] = useState<any>({});
-  const [imagesHome, setImagesHome] = useState<any>({});
-  useEffect(() => {
-    const getDataCTAHome = async () => {
-      try {
-        const res = await fetchData(
-          `${baseUrlApi}/cta-homepage?populate=bg_cta_home`
-        );
-        console.log(res.attributes);
-        setDataHome(res.attributes);
-        setImagesHome(res.attributes.bg_cta_home.data.attributes);
-      } catch (error) {
-        throw new Error(`${error}`);
-      }
-    };
-    getDataCTAHome();
-  }, []);
-  return { dataHome, imagesHome };
-};
+// export const fetchDataCtaHome = () => {
+//   const [dataHome, setDataHome] = useState<any>({});
+//   const [imagesHome, setImagesHome] = useState<any>({});
+//   useEffect(() => {
+//     const getDataCTAHome = async () => {
+//       try {
+//         const res = await fetchData(
+//           `${baseUrlApi}/cta-homepage?populate=bg_cta_home`
+//         );
+//         console.log(res.attributes);
+//         setDataHome(res.attributes);
+//         setImagesHome(res.attributes.bg_cta_home.data.attributes);
+//       } catch (error) {
+//         throw new Error(`${error}`);
+//       }
+//     };
+//     getDataCTAHome();
+//   }, []);
+//   return { dataHome, imagesHome };
+// };
 
 // logic get data content cta smes xperience
-export const fetchDataCtaSmesXperience = () => {
-  const [dataSmesXpeerience, setDataSmesXpeerience] = useState<any>({});
-  const [imagesSmesXpeerience, setImagesSmesXpeerience] = useState<any>({});
-  useEffect(() => {
-    const getDataCTAXperience = async () => {
-      try {
-        const res = await fetchData(
-          `${baseUrlApi}/cta-smes-xperience?populate=bg_cta_smes_xperience`
-        );
-        console.log(res.attributes);
-        setDataSmesXpeerience(res.attributes);
-        setImagesSmesXpeerience(
-          res.attributes.bg_cta_smes_xperience.data.attributes
-        );
-      } catch (error) {
-        throw new Error(`${error}`);
-      }
-    };
-    getDataCTAXperience();
-  }, []);
-  return { dataSmesXpeerience, imagesSmesXpeerience };
-};
+// export const fetchDataCtaSmesXperience = () => {
+//   const [dataSmesXpeerience, setDataSmesXpeerience] = useState<any>({});
+//   const [imagesSmesXpeerience, setImagesSmesXpeerience] = useState<any>({});
+//   useEffect(() => {
+//     const getDataCTAXperience = async () => {
+//       try {
+//         const res = await fetchData(
+//           `${baseUrlApi}/cta-smes-xperience?populate=bg_cta_smes_xperience`
+//         );
+//         console.log(res.attributes);
+//         setDataSmesXpeerience(res.attributes);
+//         setImagesSmesXpeerience(
+//           res.attributes.bg_cta_smes_xperience.data.attributes
+//         );
+//       } catch (error) {
+//         throw new Error(`${error}`);
+//       }
+//     };
+//     getDataCTAXperience();
+//   }, []);
+//   return { dataSmesXpeerience, imagesSmesXpeerience };
+// };
 
 // logic get data content cta smes people
-export const fetchDataCtaSmesPeople = () => {
-  const [dataSmesPeople, setDataSmesPeople] = useState<any>({});
-  const [imagesSmesPeople, setImagesSmesPeople] = useState<any>({});
-  useEffect(() => {
-    const getDataCTAPeople = async () => {
-      try {
-        const res = await fetchData(
-          `${baseUrlApi}/cta-smes-people?populate=bg_cta_smes_people`
-        );
-        console.log(res.attributes);
-        setDataSmesPeople(res.attributes);
-        setImagesSmesPeople(res.attributes.bg_cta_smes_people.data.attributes);
-      } catch (error) {
-        throw new Error(`${error}`);
-      }
-    };
-    getDataCTAPeople();
-  }, []);
-  return { dataSmesPeople, imagesSmesPeople };
-};
+// export const fetchDataCtaSmesPeople = () => {
+//   const [dataSmesPeople, setDataSmesPeople] = useState<any>({});
+//   const [imagesSmesPeople, setImagesSmesPeople] = useState<any>({});
+//   useEffect(() => {
+//     const getDataCTAPeople = async () => {
+//       try {
+//         const res = await fetchData(
+//           `${baseUrlApi}/cta-smes-people?populate=bg_cta_smes_people`
+//         );
+//         console.log(res.attributes);
+//         setDataSmesPeople(res.attributes);
+//         setImagesSmesPeople(res.attributes.bg_cta_smes_people.data.attributes);
+//       } catch (error) {
+//         throw new Error(`${error}`);
+//       }
+//     };
+//     getDataCTAPeople();
+//   }, []);
+//   return { dataSmesPeople, imagesSmesPeople };
+// };
 
-export const fetchDataHeroAbout = () => {
+// export const fetchDataHeroAbout = () => {
   
-}
+// }
